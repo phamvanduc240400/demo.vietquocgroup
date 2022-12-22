a3845197e377e56adfbd20e38199972dc54f8627
duc <ducpham@gmail.com>
2022-12-23 03:24:52 +0700

    first commit

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -3,7 +3,16 @@ import './Footer.css'
 export default class FooterHome extends Component {
   render() {
     return (
-      <div className='wrap_footer'></div>
+      <div className='wrap_footer'>
+        <div className='wrap_img_ft'>
+            <div className='img_ft1'>
+                <img style={{width:'100%', height:'100%'}} src='../img/Capture1.JPG' alt=''></img>
+            </div>
+            <div className='img_ft2'>
+                <img style={{width:'100%', height:'100%'}} src='../img/Capture2.JPG' alt=''></img>
+            </div>
+        </div>
+      </div>
     )
   }
 }
