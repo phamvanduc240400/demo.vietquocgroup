c870297d456b855ecb49fb756fc6551d1da1e586
duc <ducpham@gmail.com>
2022-12-22 19:33:45 +0700

    first commit

--- src/Header/HeaderHome.js
+++ src/Header/HeaderHome.js
@@ -1,9 +1,9 @@
 
 import React, { Component } from 'react'
 import './Header1.css'
-
 export default class HeaderHome extends Component {
-  render() {
+  render(){ 
+
     return (
       <>      
         <div className='header1'>
@@ -13,10 +13,49 @@ export default class HeaderHome extends Component {
                     <img src='../logo/logoviva.png' alt=''/>
                   </div>
                   <div className='menu'>
-                    <ul>
-                      <li><a href='@'>VỀ VIỆT QUỐC</a></li>
-                      <li><a href='@'>LĨNH VỰC HOẠT ĐỘNG</a></li>
-                      <li><a href='@'>DỰ ÁN</a></li>
+                    <ul className='menu2'>
+                      <li><a href='@'>VỀ VIỆT QUỐC</a>                   
+                      </li>
+                      <li><a href='@'>LĨNH VỰC HOẠT ĐỘNG</a>
+                       <ul className='sub_menu'>
+                          <li style={{padding: '9% 20%' , color:'#fff'}}>
+                            <ul style={{display:'flex', justifyContent:'space-evenly', marginTop:'15px'}}>
+                              <li style={{ width:'300px', height:'200px', textAlign:'center', padding:'0px'}}>
+                                 <img style={{width:'100%', height:'100%'}} src='../img/header1.jpg' alt=''></img>
+                                Xây dựng công trình
+                              </li>
+                             <li style={{ width:'300px', height:'200px', textAlign:'center', padding:'0px'}}>
+                                 <img style={{width:'100%', height:'100%'}} src='../img/header2.jpg' alt=''></img>
+                                Thiết kế kiến trúc & nội thất
+                              </li>
+                              <li style={{ width:'300px', height:'200px', textAlign:'center', padding:'0px'}}>
+                                 <img style={{width:'100%', height:'100%'}} src='../img/header3.jpg' alt=''></img>
+                                Thương mại & dịch vụ
+                              </li>
+                            </ul>
+                          </li>
+                        </ul>
+                      </li>
+                      <li><a href='@'>DỰ ÁN</a>
+                      <ul className='sub_menu2'>
+                          <li style={{padding: '9% 20%' , color:'#fff'}}>
+                            <ul style={{display:'flex', justifyContent:'space-evenly', marginTop:'15px'}}>
+                              <li style={{ width:'300px', height:'200px', textAlign:'center', padding:'0px'}}>
+                                 <img style={{width:'100%', height:'100%'}} src='../img/header4.jpg' alt=''></img>
+                                Công trình công cộng
+                              </li>
+                             <li style={{ width:'300px', height:'200px', textAlign:'center', padding:'0px'}}>
+                                 <img style={{width:'100%', height:'100%'}} src='../img/header5.jpg' alt=''></img>
+                                Nhà ở
+                              </li>
+                              <li style={{ width:'300px', height:'200px', textAlign:'center', padding:'0px'}}>
+                                 <img style={{width:'100%', height:'100%'}} src='../img/header6.jpg' alt=''></img>
+                                Nội thất
+                              </li>
+                            </ul>
+                          </li>
+                        </ul>
+                      </li>
                       <li><a href='@'>TIN TỨC</a></li>
                       <li><a href='@'>LIÊN HỆ</a></li>
                     </ul>
